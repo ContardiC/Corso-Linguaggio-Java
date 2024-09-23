41e38abf13b55b57b2e81d83ca204eaf0b4279af
Carlo Contardi <carlocontardi@MacBook-Pro-di-Carlo.local>
2024-09-23 13:27:58 +0200

    update Punto.java

--- Lezioni/Lezione1/Punto.java
+++ Lezioni/Lezione1/Punto.java
@@ -1,11 +1,54 @@
 class Punto{
-    private float x;
     private float y;
+    private float x;
+    private String name;
     // Costruttori
-    
+    Punto(float x, float y){
+        this.x=x;
+        this.y=y;
+    }
+    Punto(float x, float y, String name){
+        this.x=x;
+        this.y=y;
+        this.name=name;
+    }
+    // Costruttore vuoto
+    Punto(){
+        this.x=0;
+        this.y=0;
+    }
+    // Costruttore copia
+    Punto(Punto p){
+        this.x=p.getX();
+        this.y=p.getY();
+    }
     // Metodi
 
-    //  Getters and Setters
-    
-    // distanza tra due punti 
+        //  Getters 
+        public float getX(){
+            return this.x;
+        }
+        public float getY(){
+            return this.y;
+        }
+        public String getName(){
+            return this.name;
+        }
+        // Setters
+        public void setX(float x){
+            this.x=x;
+        }
+        public void setY(float y){
+            this.y=y;
+        }
+        public boolean setName(String name){
+            if(name.length()==0){
+                return false;
+            }
+            this.name=name;
+            return true;
+        }
+
+        // distanza tra due punti 
+        
 }
---
+++ Lezioni/Lezione1/TestPunto.java
@@ -0,0 +1,9 @@
+public class TestPunto {
+    public static void main(String[] args) {
+        Punto punto = new Punto(3,4);
+        punto.setX(5);
+        System.out.println(punto.getX());
+        Punto punto2 = new Punto(3,5,"A");
+        
+    }
+}
